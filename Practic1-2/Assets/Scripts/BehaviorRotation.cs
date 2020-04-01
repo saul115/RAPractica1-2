9bb05029a5cd1a65fd19252518a936cc52e431f5
Saul Augusto Gasca Farrera <sgfarrera@gmail.com>
2020-03-31 18:02:10 -0600

    Project Finished

---
+++ Practic1-2/Assets/Scripts/BehaviorRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorRotation : MonoBehaviour
+{
+
+    Transform rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.Rotate(Vector3.forward * 4.0f);
+    }
+}
